b8d4a908f514df867ad6c555404254744924083e
petr-tik <petr-tik@users.noreply.github.com>
2017-06-15 23:30:22 +0100

    Added sheets
    made a class to start a pyghseets connection with a google sheet and open 2 worksheets to store data in
    reuse methods to prepare and upload a row with relevant information
    used in run.py and will take a dictionary built by scrape and directions

---
+++ sheets.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python3
+
+import json
+import logging
+
+
+import pygsheets
+from pygsheets import *
+from requests.exceptions import ConnectionError
+from datetime import date
+
+
+class WSheet(pygsheets.Worksheet):
+
+    def __init__(self, worksheet_id=None, sheet_title=None):
+        logging.basicConfig(filename="app.log", level=logging.DEBUG)
+        self.logger = logging.getLogger("Flat hunter bot")
+        self.logger.info("Trying to connect and authorise")
+        try:
+            authorised = pygsheets.authorize(
+                service_file='./app/service_creds.json')
+            self.logger.info("Connected and authorised")
+            self.connected = True
+        except ConnectionError:
+            self.logger.error("Couldn't connect and authorise")
+            self.connected = False
+        if worksheet_id == None:
+            with open("./app/g_sheets_creds.json") as f:
+                data = json.load(f)
+                self.worksheet_id = data["G_SHEETS_ID"]
+        gc = authorised.open_by_key(self.worksheet_id)
+        if sheet_title == None:
+            self.sheet_title = "flats"
+        else:
+            self.sheet_title = sheet_title
+        self.flat_wks = gc.worksheet_by_title(self.sheet_title)
+        self.rent_wks = gc.worksheet_by_title("rents")
+
+    def find_header(self):
+        """
+        Header will always be top row.
+
+        Why this method:
+        way to insert the right fields into the right cells. 
+        Allows users to rearrange columns manually.
+
+        Returns header array, where the string at index idx 
+        is the column name of the column idx.
+        """
+        row = 1
+        header = self.flat_wks.get_row(row)
+        return header
+
+    def upload_flat(self, flat_res):
+        """
+        Input:
+            - flat result dictionary
+        Returns nothing and inserts row to worksheet
+        """
+        row = self._prepare_row_for_upload(flat_res)
+        self.flat_wks.insert_rows(self.flat_wks.rows, values=row, inherit=True)
+
+    def _prepare_row_for_upload(self, flat_res, header=None):
+        """
+        Input:
+            - flat_result dictionary key: value 
+
+        Returns:
+            - array of strings converted to utf-8 
+             (fully complaint with http request spec) with errors replaced.
+        Selects the correct cell for each key and returns a row
+        """
+        if header == None:
+            header = self.find_header()
+        row = []
+        for idx, item in enumerate(header):
+            if item in ["Date_added", "Date_updated"]:
+                continue
+            item_to_add = flat_res[item]
+            if isinstance(item_to_add, bytes):
+                item_to_add = item_to_add.decode(
+                    encoding="utf-8", errors="replace")
+            row.append(item_to_add)
+        Date_added = date.today().strftime("%d-%m-%Y")
+        # add date added for the update function
+        row.append(Date_added)
+        return row
+
+    def get_flats(self):
+        """ 
+        Used for updating current flats. 
+        Returns a dictionary
+            key: flat url
+            value: rent_monthly
+        """
+        pass
+
+
+def test():
+    w = WSheet()
+    w.find_header()
+
+
+if __name__ == "__main__":
+    test()
